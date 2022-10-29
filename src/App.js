61c148d3fb749246a75718cdfe02d81d81cdba5b
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-10-28 21:55:35 -0300

    resolucao em aula

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import './style.css';
 
+const filmes = [
+  {
+    nome: 'O Senhor do Aneis: A Sociedade do Anel',
+    lancamento: 2002,
+    oscar: true,
+    diretores: ['Peter Jackson'],
+    generos: ['Fantasia', 'Aventura'],
+  },
+  {
+    nome: 'Harry Potter e a Pedra Filosofal',
+    lancamento: 2001,
+    oscar: true,
+    diretores: ['Chris Columbus'],
+    generos: ['Fantasia'],
+  },
+  {
+    nome: 'Matrix',
+    oscar: true,
+    lancamento: 1999,
+    diretores: ['Lana Wachowski', 'Lilly Wachowski'],
+    generos: ['Ação', 'Ficção Cientifica'],
+  },
+  {
+    nome: 'Meninas Malvadas',
+    oscar: false,
+    lancamento: 2004,
+    diretores: ['Mark Waters'],
+    generos: ['Comédia'],
+  },
+];
+
+const titulos = filmes
+  .filter((filme) => filme.oscar)
+  .map((filme) => filme.nome)
+  .map((nome) => <h2>{nome}</h2>);
+
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  return <div>{titulos}</div>;
 }
