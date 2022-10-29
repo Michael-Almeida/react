171e753d2abb6ea200b3410f374ce2ea369b7fb4
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-10-28 22:17:16 -0300

    1 file modified, 2 added, 2 deleted

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import App from './1.7.1/1.7.1';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
