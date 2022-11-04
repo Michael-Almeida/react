65af18a639717becca2db807a092712076c43cb8
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-11-04 13:00:15 -0300

    tentativa renderização

---
+++ src/1.7.1/enunciado1.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './style.css';
+
+const filmes = [
+  {
+    nome: 'O Senhor do Aneis: A Sociedade do Anel',
+    lancamento: 2002,
+    oscar: true,
+    diretores: ['Peter Jackson'],
+    generos: ['Fantasia', 'Aventura'],
+  },
+  {
+    nome: 'Harry Potter e a Pedra Filosofal',
+    lancamento: 2001,
+    oscar: true,
+    diretores: ['Chris Columbus'],
+    generos: ['Fantasia'],
+  },
+  {
+    nome: 'Matrix',
+    oscar: true,
+    lancamento: 1999,
+    diretores: ['Lana Wachowski', 'Lilly Wachowski'],
+    generos: ['Ação', 'Ficção Cientifica'],
+  },
+  {
+    nome: 'Meninas Malvadas',
+    oscar: false,
+    lancamento: 2004,
+    diretores: ['Mark Waters'],
+    generos: ['Comédia'],
+  },
+];
+
+//Enunciado 1
+const titulos = filmes
+  .map((filme) => filme.nome)
+  .map((nome) => <h4>{nome}</h4>);
+const titulos2 = filmes
+  .map((filme) => filme.diretores)
+  .map((diretores) => <h5>{diretores}</h5>);
+
+export default function App() {
+  return (
+    <div>
+      {titulos}
+      {titulos2}
+    </div>
+  );
+}
+
+/* const titulos = filmes
+  .map((filme) => (filme.nome)&& (filme.diretores))
+  //.map((filme)=>filme.diretores)
+  .map((nome) => <h4>{nome}</h4>)
+  .map((diretores) => <h4>{diretores} </h4>)
+  */
