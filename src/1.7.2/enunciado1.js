446d852d1bc06026e22ddeaf0670925997bce902
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-11-04 22:24:16 -0300

    2 files modified

--- src/1.7.2/enunciado1.js
+++ src/1.7.2/enunciado1.js
@@ -1,4 +1,44 @@
 import React from 'react';
 import './style.css';
 
-export default function App() {}
+const Personagens = () => {
+  const personagens = [
+    'Jill Valentine',
+    'Claire Redfield',
+    'Ada Wong',
+    'Chris Redfield',
+    'Leon S. Kennedy',
+    'Albert Wesker',
+  ];
+
+  const personagensFiltrados = personagens
+    .filter((personagem) => {
+      const EhdaFamilia = personagens.includes('Redfield');
+      return EhdaFamilia;
+    })
+    .map((props) => {
+      const { personagens } = props;
+      return (
+        <ul>
+          {personagens.map((personagem) => (
+            <li>{personagem}</li>
+          ))}
+        </ul>
+      );
+    });
+
+  return (
+    <div>
+      <h3>personagensFiltrados</h3>
+      <ul>
+        {personagens.map((personagem) => (
+          <li>{personagensFiltrados}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default function App() {
+  return <Personagens />;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './1.7.1/enunciado1';
+import App from './1.7.2/enunciado1';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
