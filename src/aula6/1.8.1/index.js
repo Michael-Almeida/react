b341ab585049d4d1391a9dc3c79053b2a7a7dd35
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-11-25 20:50:27 -0300

    2 files modified, 2 added

--- src/aula6/1.8.1/index.js
+++ src/aula6/1.8.1/index.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import './style.css';
 
+const Personagens = () => {
+  const personagens = [
+    'Jill Valentine',
+    'Jack Baker',
+    'Claire Redfield',
+    'Ada Wong',
+    'Chris Redfield',
+    'Leon S. Kennedy',
+    'Albert Wesker',
+    'Lucas Baker',
+  ];
+
+  export const BotaoPersonagens = (props) => {
+    const { personagem } = props;
+    return (
+      <div>
+        <h3>Personagens de Resident Evil</h3>
+        <ul>
+          {personagens.map((personagem) => (
+            <li>{personagem}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+};
+
 export default function App() {
-  return;
+  return <BotaoPersonagens />;
 }
