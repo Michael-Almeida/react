94030fc09a4bd11f86494308be58f6e583e3cbb6
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-11-25 22:25:19 -0300

    exercicio em desenvolvimento

---
+++ src/aula6/1.8.1/index2.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useState } from 'react';
+import './style.css';
+
+const Personagens = () => {
+  const _personagens = [
+    'Jill Valentine',
+    'Jack Baker',
+    'Claire Redfield',
+    'Ada Wong',
+    'Chris Redfield',
+    'Leon S. Kennedy',
+    'Albert Wesker',
+    'Lucas Baker',
+  ];
+
+  const [personagens, setPersonagen] = useState(_personagens);
+
+  const filtrarFamiliaBacker = () => {
+    _personagens.filter((personagem) => {
+      const ehDaFamilia = personagem.includes('Baker');
+      setPersonagen(ehDaFamilia);
+    });
+  };
+
+  const filtrarFamiliaRedfield = () => {
+    _personagens.filter((personagem) => {
+      const ehDaFamilia = personagem.includes('Redfield');
+      setPersonagen(ehDaFamilia);
+    });
+  };
+
+  return (
+    <div>
+      <h3>Personagens de Resident Evil</h3>
+      <button onClick={filtrarFamiliaBacker}>Familia Backer</button>
+      <button onClick={filtrarFamiliaRedfield}>Familia Redfield</button>
+      <ul>
+        {personagens.map((personagem) => (
+          <li>{personagem}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default function App() {
+  return <Personagens />;
+}
